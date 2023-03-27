30a60beae3a8ca3c51eab5082b6fd54ea9ccb11d
Holylogus <hamar.patrik94@gmail.com>
2023-03-27 20:09:18 +0200

    header elkészítése

--- forras.js
+++ forras.js
@@ -1,6 +1,16 @@
 export const HATTERKEPEK = [
     "Kepek/Backgrounds/E10_fashion.PNG",
     "Kepek/Backgrounds/E10_fashion1.PNG",
-    "Kepek/Backgrounds/F06_landing4.PNG",
+    "Kepek/Backgrounds/F06_landing3.PNG",
     "Kepek/Backgrounds/MLIB_landing3.PNG"
+]
+
+export const LANDIINGS = [{
+    szoveg: "Kezdőlap",
+    source: "index.html"   
+},
+{
+    szoveg: "Termékek",
+    source: "products.html"}
+
 ]
