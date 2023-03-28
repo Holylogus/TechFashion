a08fe98a8fc17a73e9e56d3fabace40924f11408
Holylogus <hamar.patrik94@gmail.com>
2023-03-28 16:28:09 +0200

    article kártyák létrehozása, rendezése

--- forras.js
+++ forras.js
@@ -2,7 +2,7 @@ export const HATTERKEPEK = [
     "Kepek/Backgrounds/E10_fashion.PNG",
     "Kepek/Backgrounds/E10_fashion1.PNG",
     "Kepek/Backgrounds/F06_landing3.PNG",
-    "Kepek/Backgrounds/MLIB_landing3.PNG"
+    "Kepek/Backgrounds/E10_fashion2.PNG"
 ]
 
 export const LANDIINGS = [{
@@ -12,5 +12,37 @@ export const LANDIINGS = [{
 {
     szoveg: "Termékek",
     source: "products.html"}
+]
 
+export const TERMÉKEK = [
+    {
+        model: "F06",
+        content: "Smart audio szemüveg",
+        price: "10.490,-Ft",
+        imgSource: "Kepek/F06_audio/F06_fashion1.PNG"
+    },
+    {
+        model: "X11",
+        content: "Smart audio szemüveg",
+        price: "15.790,-Ft",
+        imgSource: "Kepek/X11_audio/X11_black.PNG"
+    },
+    {
+        model: "E10",
+        content: "Smart audio szemüveg",
+        price: "23.490,-Ft",
+        imgSource: "Kepek/E10/E10.PNG"
+    },
+    {
+        model: "MLIB",
+        content: "Smart audio szemüveg",
+        price: "21.550,-Ft",
+        imgSource: "Kepek/MLIB/MLIB_landing.PNG"
+    },
+    {
+        model: "CY03",
+        content: "Smart audio szemüveg",
+        price: "17.990,-Ft",
+        imgSource: "Kepek/CY13/CY03_landing.PNG"
+    }
 ]
--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-import { HATTERKEPEK } from "./forras.js";
-import { LANDIINGS } from "./forras.js";
+import { HATTERKEPEK, LANDIINGS, TERMÉKEK } from "./forras.js";
 
 window.addEventListener("load", init);
 
 const HEADER = document.querySelector("header");
+const ARTICLE = document.querySelector("article")
 
 function init() {
-  Header();
+  header();
+  article();
 }
 
-function Header() {
-  NavBar();
+function header() {
+  navBar();
 
   //MARKETING
   let marketingSzoveg = document.createElement("h1");
@@ -59,13 +60,13 @@ gombokDiv.appendChild(forwardButton)
 
 
 }
-function NavBar() {
-  const Navbar = document.createElement("div");
-  Navbar.classList.add("navbar");
-  HEADER.appendChild(Navbar);
+function navBar() {
+  const NAVBAR = document.createElement("div");
+  NAVBAR.classList.add("navbar");
+  HEADER.appendChild(NAVBAR);
   const LandingPages = document.createElement("ul");
-  Navbar.appendChild(LandingPages);
-  console.log(Navbar);
+  NAVBAR.appendChild(LandingPages);
+  console.log(NAVBAR);
   for (let index = 0; index < LANDIINGS.length; index++) {
     let liElements = document.createElement("li");
     LandingPages.appendChild(liElements);
@@ -76,12 +77,52 @@ function NavBar() {
   }
   let vendor = document.createElement("h1");
   vendor.textContent = "TechFashion";
-  Navbar.appendChild(vendor);
-  const Cart = document.createElement("button");
-  Cart.textContent = "Kosár";
-  Navbar.appendChild(Cart);
+  NAVBAR.appendChild(vendor);
+  const CART = document.createElement("button");
+  CART.textContent = "Kosár";
+  NAVBAR.appendChild(CART);
   let iconCart = document.createElement("img");
   iconCart.src = "Icons/cart.png";
   iconCart.alt = "kosár";
-  Cart.appendChild(iconCart);
+  CART.appendChild(iconCart);
+}
+
+function article() {
+
+  //KIEMELT TERMÉKEK - FELIRAT
+  let articleContainer = document.createElement("div") 
+  articleContainer.classList.add("article-container") 
+  let kiemeltTermékekContainer = document.createElement("div")
+  let kiemeltTermékek = document.createElement("h1")
+  kiemeltTermékek.textContent = "Kiemelt Termékek"
+  kiemeltTermékekContainer.appendChild(kiemeltTermékek)
+  articleContainer.appendChild(kiemeltTermékekContainer)
+  ARTICLE.appendChild(articleContainer)
+
+  //KIEMELT TERMÉKEK - KÁRTYÁK
+  let cards = document.createElement("div")
+  cards.classList.add("cards")
+  articleContainer.appendChild(cards)
+
+  for (let index = 0; index < TERMÉKEK.length; index++) {
+    let cardContainer = document.createElement("div")
+    cardContainer.classList.add("productCard")
+    let cardImage = document.createElement("div")
+    cardImage.classList.add("productImg")
+    cardImage.style.height = "300px"
+    cardImage.style.width = "200px"
+    cardImage.style.backgroundImage = `url(${TERMÉKEK[index].imgSource})` 
+    let cardText = document.createElement("p")
+    cardText.textContent = `${TERMÉKEK[index].model} ${TERMÉKEK[index].content}`
+    let cardPrice = document.createElement("h3")
+    cardPrice.textContent = `${TERMÉKEK[index].price}`
+    cardContainer.appendChild(cardImage)
+    cardContainer.appendChild(cardText)
+    cardContainer.appendChild(cardPrice)
+    cards.appendChild(cardContainer)
+  }
+  articleContainer.appendChild(cards)
+
+
+
 }
